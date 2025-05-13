ed63420ecfa3c427287c159d20f84917cddc24c8
21017511vocongtuananh <127751619+21017511vocongtuananh@users.noreply.github.com>
2025-05-13 11:18:14 +0700

    sua backend

--- Backend/src/main/java/com/Chat/Chat/config/WebSocketConfig.java
+++ Backend/src/main/java/com/Chat/Chat/config/WebSocketConfig.java
@@ -40,7 +40,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-				.setAllowedOrigins("http://localhost:3000")
+				.setAllowedOrigins("http://103.90.226.183:3000")
 				.withSockJS();
 	}
 
