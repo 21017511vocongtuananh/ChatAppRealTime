8bac67f2c5897c34c89f7219b67d6445f27b4802
21017511vocongtuananh <127751619+21017511vocongtuananh@users.noreply.github.com>
2025-05-27 06:29:32 +0700

    finanl

--- Backend/src/main/java/com/Chat/Chat/config/WebSocketConfig.java
+++ Backend/src/main/java/com/Chat/Chat/config/WebSocketConfig.java
@@ -40,7 +40,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-				.setAllowedOrigins("https://nhom10iuh.id.vn","http://localhost:8081")
+				.setAllowedOrigins("https://nhom10iuh.id.vn","http://localhost:8081","http://localhost:3000")
 				.withSockJS();
 	}
 
